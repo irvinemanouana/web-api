65defd9f61d58f6369b6e115337fc3cd2b161da8
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2016-01-21 22:38:59 +0100

    using promise instead of callback

--- server/actions/category/remove.js
+++ server/actions/category/remove.js
@@ -5,22 +5,41 @@ module.exports = function(app) {
         var categoryId = req.params.id;
 
         if ( !categoryId ) {
-            return res.status(500).send({ error : 'check parameter' });
+            return res.status(500).send({ error : 'check parameter in url' });
         }
         else {
-            app.models.Category.findByIdAndRemove(categoryId,
-                function(err, instance) {
-                    if (err) {
-                        return res.status(500).json({ error : err });
-                    }
-                    else if ( !instance ) {
-                        return res.status(404).json({ error : 'Category not found' });
-                    }
-                    else {
-                        res.json(instance);
-                    }
+            var promise             = app.models.Category.findById(categoryId).exec(),
+                categoryToRemove    = undefined;
+
+            promise.then(function (instance) {
+                if ( !instance ) {
+                    return res.status(404).json({ error : 'Category not found' });
                 }
-            );
+                else {
+                    categoryToRemove = instance;
+                    return app.models.Event.findOne({category : instance}).exec();
+                }
+            })
+            .then(function (instance) {
+                if (instance) {
+                    return res.status(500).json({ error : 'Cannot to remove category because it is used by events' });
+                }
+                else {
+                    return categoryToRemove.remove();
+                }
+            })
+            .then(function (instance) {
+                return app.models.Category.findById(categoryId).exec();
+            })
+            .then(function (instance) {
+                if ( !instance ) {
+                    res.json(categoryToRemove);
+                }
+                else {
+                    return res.status(500).json({ error : 'Cannot to remove category' });
+                }
+            })
+            ;
         }
     }
 };
