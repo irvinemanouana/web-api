6436e6b40ee8b71bb21afb350cc4d26da2432c53
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2016-01-23 11:18:48 +0100

    check if string is empty

--- server/actions/auth/login.js
+++ server/actions/auth/login.js
@@ -5,44 +5,49 @@ module.exports = function(app) {
         var username = req.body.username,
             password = req.body.password;
 
-        app.models.User.findOne(
-        {
-            $or:[ 
-                {
-                    username: username,
-                    password: password
-                },
-                {
-                    username: username
+        if ( !username || app.utils.isEmpty(username) || !password || app.utils.isEmpty(password) ) {
+            return res.status(500).send({ error : 'check body parameter' });
+        }
+        else {
+            app.models.User.findOne(
+            {
+                $or:[ 
+                    {
+                        username: username,
+                        password: password
+                    },
+                    {
+                        username: username
+                    }
+                ]
+            })
+            .select('+password')
+            .exec(function(err, instance) {
+                if (err) {
+                    return res.status(500).json({ error : err });
                 }
-            ]
-        })
-        .select('+password')
-        .exec(function(err, instance) {
-            if (err) {
-                return res.status(500).json({ error : err });
-            }
-            else if ( !instance ) {
-                return res.status(404).json({ error : 'Account not found' });
-            }
-            else if (instance.password != password) {
-                return res.status(401).json({ error : 'Unauthorized' });
-            }
-            else {
-                app.models.User.findOne(
-                {
-                    username: username,
-                    password: password
-                }, function(err, toReturn) {
-                    if (err) {
-                        return res.status(500).json({ error : err });
-                    }
-                    else {
-                        req.session.userId = toReturn.id;
-                        return res.json(toReturn);
-                    }
-                });
-            }
-        });
+                else if ( !instance ) {
+                    return res.status(404).json({ error : 'Account not found' });
+                }
+                else if (instance.password != password) {
+                    return res.status(401).json({ error : 'Unauthorized' });
+                }
+                else {
+                    app.models.User.findOne(
+                    {
+                        username: username,
+                        password: password
+                    }, function(err, toReturn) {
+                        if (err) {
+                            return res.status(500).json({ error : err });
+                        }
+                        else {
+                            req.session.userId = toReturn.id;
+                            return res.json(toReturn);
+                        }
+                    });
+                }
+            });
+        }
     }
 };
--- server/configs/utils.js
+++ server/configs/utils.js
@@ -3,3 +3,7 @@
 exports.isObjectId = function (id) {
     return id.match (/^[0-9a-fA-F]{24}$/) != null;
 };
+
+exports.isEmpty = function (str) {
+    return str === null || str.match(/^ *$/) !== null;
+};
