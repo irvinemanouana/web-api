c5476d827f4610d697c9b44feafaf8650867e80f
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2016-02-03 18:58:00 +0100

    implement OAuthRefreshToken

--- server/models/index.js
+++ server/models/index.js
@@ -5,9 +5,10 @@ var mongoose = require('mongoose');
 module.exports = function(app){
     app.mongoose = mongoose.connect(app.configs.database.uri);
 
-    app.models              = {};
-    app.models.User         = require('./User')(app);
-    app.models.Category     = require('./Category')(app);
-    app.models.Event        = require('./Event')(app);
-    app.models.OAuthClients = require('./OAuthClients')(app);
+    app.models                      = {};
+    app.models.User                 = require('./User')(app);
+    app.models.Category             = require('./Category')(app);
+    app.models.Event                = require('./Event')(app);
+    app.models.OAuthClients         = require('./OAuthClients')(app);
+    app.models.OAuthRefreshTokens   = require('./OAuthRefreshTokens')(app);
 };
