6b4e4bde37e2b8139ad98a8197c7af35f138b6ba
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2016-02-23 23:32:58 +0100

    add OAuth2 documentation

--- server/routes/oauth.js
+++ server/routes/oauth.js
@@ -5,6 +5,15 @@ var bodyparser = require('body-parser').urlencoded({ extended: true });
 
 module.exports = function(app){
 
+    /**
+     * @api {ALL} /oauth/token Authentication
+     * @apiName Authentication
+     * @apiGroup OAuth2
+     *
+     * @apiVersion 1.0.0
+     *
+     * @apiDescription OAuth2.
+     */
     router.all('/token', bodyparser, app.oauth.grant());
 
     return router;
