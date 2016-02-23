73de6aae0f92c5c08c67cc13c3db3a18c3a6d1c4
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2016-02-23 18:11:14 +0100

    update list action

--- server/actions/event/list.js
+++ server/actions/event/list.js
@@ -2,13 +2,41 @@
 
 module.exports = function(app) {
     return function(req, res, next){
-        var count   = parseInt(req.query.count),
-            page    = parseInt(req.query.page),
-            first   = ((count * page) - count);
+        var count           = parseInt(req.query.count),
+            page            = parseInt(req.query.page),
+            sort            = req.query.sort,
+            categoryId      = req.query.categoryId,
+            dateFilter      = req.query.date,
+            filter          = {};
 
-        app.models.Event.find()
-        .limit(count).skip(first)
-        .populate('category').exec()
+        if (!global.isNullOrEmpty(categoryId)) {
+            filter.category = categoryId;
+        }
+
+        if (!global.isNullOrEmpty(dateFilter)) {
+            var dateNow = new Date();
+            switch(dateFilter) {
+                case 'passed':
+                    filter.date = { $lt: dateNow};
+                    break;
+                case 'future':
+                    filter.date = { $gt: dateNow};
+                    break;
+            }
+        }
+
+        var query = app.models.Event.find(filter);
+
+        if (!isNaN(count) && !isNaN(page)) {
+            var first = ((count * page) - count);
+            query.limit(count).skip(first);
+        }
+
+        if (!global.isNullOrEmpty(sort) && (sort === 'asc' || sort === 'desc')) {
+            query.sort({ date: sort });
+        } 
+
+        query.populate('category').exec()
         .then(function(instances) {
             res.json(instances);
         })
